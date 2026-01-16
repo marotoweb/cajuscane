8acf48e1da0f7d5c5d687e14d63de2e57d3581f2
Roberto Cc <4264711+marotoweb@users.noreply.github.com>
2026-01-16 23:09:45 +0000

    Define SDK 35

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         //targetSdk = flutter.targetSdkVersion
-        targetSdkVersion = 36
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
